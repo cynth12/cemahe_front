fe0928fbea49f2f692fcfd606b41341d0699c4ba
cynth12 <gonzalez.cynth12@gmail.com>
2025-07-23 01:18:53 -0600

    todo nuevo listo

--- resources/views/layouts/commonCSS.blade.php
+++ resources/views/layouts/commonCSS.blade.php
@@ -1,21 +1,17 @@
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/responsive.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/all.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/themify-icons.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/icofont.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap-icons.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bsnav.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/preloader.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/magnific-popup.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
-
-
-
 	
 	
-
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/all.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/themify-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/icofont.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/bootstrap-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/bsnav.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/preloader.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/magnific-popup.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/swiper-bundle.min.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
 	
 	
 
